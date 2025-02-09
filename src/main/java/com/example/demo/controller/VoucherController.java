f1b95dbdf0ccfdb9f77d3ae234b768f1f135f608
DUONGNMPH34598 <duongnmph34598@fpt.edu.vn>
2025-02-09 12:27:13 +0700

    ui voucher

---
+++ src/main/java/com/example/demo/controller/VoucherController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Voucher;
+import com.example.demo.repository.VoucherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class VoucherController {
+    @Autowired
+    private VoucherRepository voucherRepository;
+
+    @GetMapping("/voucher/hien-thi")
+    public String hienThiVoucher(Model model){
+        model.addAttribute("listVoucher", voucherRepository.findAll());
+        return "/online/voucher/voucher";
+    }
+
+    @GetMapping("/voucher/view-add")
+    public String viewAdd() {
+        return "add-voucher";
+    }
+
+    @PostMapping("/voucher/add")
+    public String add(@ModelAttribute("voucher") Voucher voucher) {
+        voucherRepository.save(voucher);
+        return "redirect:/voucher/hien-thi";
+    }
+}
